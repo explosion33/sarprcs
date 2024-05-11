dfff79dff04ab2805aeb8e5bc071911cada9f27c
Jimmy Fowler <jfowler0@uw.edu>
2024-05-11 15:24:33 -0700

    functional 1D sim with plot

--- controller/RCS_PID/simulation.py
+++ controller/RCS_PID/simulation.py
@@ -1,15 +1,17 @@
 from PID import PID 
 from physics import oneDofPhysics
+import numpy as np
+import matplotlib.pyplot as plt
 
 # --INITIALIZE STATE--
-theta0 = -1 # rad
-omega0 = -3 # rad/s
+theta0 = -0.8 # rad
+omega0 = -0.5 # rad/s
 initial_state = [theta0, omega0]
 # --------------------
 
 # --SIM PARAMS-- (units in N, m, kg, s, etc.) 
 dt = 0.01 
-mmoi = 100
+mmoi = 1000
 rz = 5 # vertical distance from COM to thruster 
 sim_time = 0
 time_limit = 10
@@ -17,17 +19,17 @@ time_limit = 10
 
 # --CONTROLLER PARAMS--
 Kp = 100
-Ki = 0.1
-Kd = 0.1
+Ki = 1
+Kd = 5
 min_thrust = 5
-max_thrust = 50
+max_thrust = 100
 # ---------------------
 
 controller = PID(Kp, Ki, Kd)
 controller.setLims(min_thrust, max_thrust) # min & max thrust in newtons
 rocket = oneDofPhysics(initial_state, mmoi, rz)
 
-N = int(time_limit/dt)
+N = int(time_limit/dt)+1
 thetas = [None]*N
 omegas = [None]*N
 commands = [None]*N
@@ -36,17 +38,25 @@ while sim_time < time_limit:
     thetas[i], omegas[i] = rocket.state_vector
     command = controller.compute(rocket.state_vector[0], dt)
     commands[i] = command
-    print("theta: ", rocket.state_vector[0],"\tomega: ", rocket.state_vector[1], "\tcommand: ",command)
+    # theta_deg = rocket.state_vector[0]*360/(2*np.pi)
+    # omega_deg_s = rocket.state_vector[1]*360/(2*np.pi)
+    # print("time: ", round(sim_time,3),"s"
+    #       "\ttheta: ", round(theta_deg,3),
+    #       "\tomega: ", round(omega_deg_s,3),
+    #       "\tcommand: ", round(command,3))
     rocket.nextState(command, dt)
     sim_time += dt
     i+=1
 
-# for i,j in zip(thetas, commands):
-#     print("theta: ", i, "\tcommand: ",j)
+# --PLOTTING--
+theta_deg = [t*360/(2*np.pi) for t in thetas]
+omega_deg_s = [o*360/(2*np.pi) for o in omegas]
+time = np.linspace(0, time_limit, N)
+fig, ax = plt.subplots(2)
+ax[0].plot(time, theta_deg, 'b-')
+ax[1].plot(time, omega_deg_s,'m-')
+ax[0].set_ylabel(r"$\theta_x$", fontsize=15)
+ax[1].set_ylabel(r"$\omega_x$", fontsize=15)
 
-
-# # --PLOTTING--
-# time = range(0, time_limit, dt)
-# fig, ax = plt.subplots(2)
-# ax[0].plot(time, thetas)
-# ax[1].plot(time, omegas)
+fig.savefig("RCS_PID/figs/states_over_time")
+fig.show()
